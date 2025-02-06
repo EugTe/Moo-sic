18d589fa35261b8d59636b0d526a7b663b65b85b
EugTe <65484340+EugTe@users.noreply.github.com>
2025-02-06 05:53:21 -0800

    Fixed 'Log In' button

--- src/Header.jsx
+++ src/Header.jsx
@@ -28,9 +28,9 @@ export default function Header({ user, onLogout }) {
               </>
             ) : (
               <button
-                onClick={() =>
-                  (window.location.href = '${API_URL}/auth/login')
-                }
+                onClick={() => {
+                  window.location.href = `${API_URL}/auth/login`;
+                }}
                 className="bg-spotify-green hover:bg-spotify-green/80 text-white px-4 py-2 rounded"
               >
                 Login with Spotify
